d04a69ccd57d33481e74f5b1d290929eebf83180
alidaomy <alidaomy@mail.ru>
2025-12-06 17:23:10 +0300

    создание тестовых файлов и меню

--- Adamenko_team_sorts.cpp
+++ Adamenko_team_sorts.cpp
@@ -124,9 +124,71 @@ void testSort(const string& name, Node<T>* (*sortFunc)(Node<T>*), Node<T>* head)
 
 //Лиза
 
+
+// Генерация тестовых данных
+void generateDataFile(const string& filename, int type) {
+    ofstream file(filename);
+    srand(time(0));
+
+    switch (type) {
+    case 1:
+        for (int i = 0; i < 1000; i++) {
+            file << rand() % 10000 - 5000 << " ";
+        }
+        cout << "Создан файл data1.txt с 1000 целых чисел" << endl;
+        break;
+
+    case 2:
+        for (int i = 0; i < 10000; i++) {
+            file << rand() % 10000 - 5000 << " ";
+        }
+        cout << "Создан файл data2.txt с 10000 целых чисел" << endl;
+        break;
+
+    case 3:
+    {
+        for (int i = 0; i < 1000; i++) {
+            char letter = 'a' + rand() % 26;
+            file << letter << " ";
+        }
+        cout << "Создан файл data3.txt с 1000 английских букв" << endl;
+        break;
+    }
+
+    case 4:
+        for (int i = 1000; i >= 1; i--) {
+            file << i << " ";
+        }
+        cout << "Создан файл data4.txt с 1000 чисел (1000..1)" << endl;
+        break;
+
+    case 5:
+        for (int i = 0; i < 1000; i++) {
+            float val = (rand() % 10000 - 5000) / 100.0f;
+            file << val << " ";
+        }
+        cout << "Создан файл data5.txt с 1000 дробных чисел" << endl;
+        break;
+    }
+
+    file.close();
+}
+
+void showMenu() {
+    cout << "\nТЕСТИРОВАНИЕ СОРТИРОВОК НА СПИСКАХ" << endl;
+    cout << "1. 1000 целых чисел (data1.txt)" << endl;
+    cout << "2. 10000 целых чисел (data2.txt)" << endl;
+    cout << "3. 1000 букв (data3.txt)" << endl;
+    cout << "4. 1000 чисел в обратном порядке (data4.txt)" << endl;
+    cout << "5. 1000 дробных чисел (data5.txt)" << endl;
+    cout << "6. Выход" << endl;
+    cout << "Ваш выбор (1-6): ";
+}
+
 int main(){
 
 	setlocale(LC_ALL, "Russian");
+
 
 
 
